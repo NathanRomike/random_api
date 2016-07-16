a35befc0e2b297f3b57219bdf51cfb008e2d9792
NathanRomike <nathanromike@gmail.com>
2016-07-15 22:18:09 -0700

    Wrap random numbers in functions.

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var express = require('express');
 
 var app = express();
 
-var randomNumber = Math.random() * (99999 - 2) + 2;
+function getRandomNumber() {
+  return Math.floor(Math.random() * (99999 - 2)) + 2;
+};
 
-var ramdomMultiple = Math.floor(Math.random() * (10 - 2)) + 2;
+function getMultiple() {
+  return Math.floor(Math.random() * (10 - 2)) + 2;
+}
 
 app.get('/random', function(req, res) {
   res.json({
-    1: randomNumber * ramdomMultiple,
-    2: randomNumber,
-    3: ramdomMultiple
+    1: getRandomNumber() * getMultiple(),
+    2: getRandomNumber(),
+    3: getMultiple()
   });
 });
 
